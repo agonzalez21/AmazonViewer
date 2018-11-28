a8dab492eaa7716463af3336bfb5ad608196af0e
Ing. Alberto González Balderas <albertoglezb@hotmail.com>
2018-11-28 07:01:03 -0600

    Se creó el método para crear books.

--- src/com/platzi/amazonviewer/model/Book.java
+++ src/com/platzi/amazonviewer/model/Book.java
@@ -1,15 +1,16 @@
 package com.platzi.amazonviewer.model;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Book extends Redaccion{
-	
+
 	private int id;
 	private String isbn;
 	private boolean readed;
 	private int timeReaded;
-	
+
 	public int getId() {
 		return id;
 	}
@@ -40,8 +41,26 @@ public class Book extends Redaccion{
 
 	public static ArrayList<Book> makeBooks() {
 		ArrayList<Book> books = new ArrayList<Book>();
-		
+
+		for (int index = 0; index <= 5; index++) {
+			int year = 2003+index;
+			int month = 12;
+			int day = 12;
+			String date = year + "/" + month +"/" + day;
+			java.util.Date utilDate=null;
+
+
+			try {
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+				utilDate = formatter.parse(date);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+
+			books.add(new Book("Book"+index,utilDate,"Editorial"+index,new String[1]));
+		}
+
 		return books;
 	}	
-	
+
 }
